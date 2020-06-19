1810499e3e488572c7c1ba262a1b96d530083e0b
Dmitrii Elkin <7777632@gmail.com>
2020-06-19 10:02:58 +0300

    update config

--- config.example.php
+++ config.example.php
@@ -8,10 +8,12 @@
 return [
     'env' => 'prod', // prod | dev
     'attachmentsDir' => '/app/tmp/attachments',
+    'telegramToken' => 'XXX',
     'testMailBox' => [
 //        'imapPath' => '{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX',
         'imapPath' => '{imap.gmail.com:993/imap/ssl}INBOX',
         'login' => 'mail2telegram.app@gmail.com',
         'pwd' => 'XXXX',
+        'chatId' => 123456,
     ]
 ];
