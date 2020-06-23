14267bb764e073b6bc381dc75d53c2c4ddfbb7be
Dmitrii Elkin <7777632@gmail.com>
2020-06-23 12:42:48 +0300

    add monolog cli formatter

--- config.example.php
+++ config.example.php
@@ -1,6 +1,11 @@
 <?php
 
+/** @noinspection PhpFullyQualifiedNameUsageInspection */
+
 use App\Model\Account;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
 
 return [
     'env' => 'prod', // prod | dev
@@ -10,6 +15,11 @@ return [
             new Account('{imap.gmail.com:993/imap/ssl}INBOX', 'mail2telegram.app@gmail.com', 'XXX', 123456),
         ],
     ],
+    LoggerInterface::class => static function () {
+        $stream = new StreamHandler(STDERR);
+        //$stream->setFormatter(new \Dev\CliFormatter());
+        return (new Logger('app'))->pushHandler($stream);
+    },
 ];
 
 // Для Gmail нужно включить "less secured apps"
